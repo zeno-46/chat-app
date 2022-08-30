dc5726c7366202c6dbeab1161e56aaf43c52e699
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-08-30 15:12:59 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
+
+  def index
+  end
+  
   def new
     @room = Room.new
   end
@@ -11,7 +15,7 @@ class RoomsController < ApplicationController
       render :new
     end
   end
-  
+
   private
 
   def room_params
